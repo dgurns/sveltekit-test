2c6be12b2f00f1c3d82dfdbe6367257b4c3a3fb3
Dan Gurney <gurney.dan@gmail.com>
2021-11-20 16:35:42 +0000

    Initial commit

---
+++ src/routes/test.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Test</title>
+</svelte:head>
+
+<div class="content">
+	<h1>Test route</h1>
+
+	<p>
+		Here's a test page<br />
+		<a href="/about">Go to About page</a>
+	</p>
+</div>
+
+<style>
+	.content {
+		background-color: lightblue;
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
